943732e68f8bd7548accf95f4b4efba47a43161d
anoukmartin <anouk.mrtn@gmail.com>
2023-09-09 19:40:28 +0000

    Réglages des options visuelles ggplot et gtsummary

--- Code/packages.R
+++ Code/packages.R
@@ -7,8 +7,27 @@ library(haven)
 library(git2r)
 library(stringr)
 library(aws.s3)
-library(gtsummary)
+
 library(dplyr)
 library(forcats)
 library(tidyr)
 
+# Mise en forme des tableaux
+library(gtsummary)
+theme_gtsummary_compact()
+theme_gtsummary_continuous2(set_theme = TRUE)
+theme_gtsummary_language(language = "fr", decimal.mark = ",", big.mark = " ")
+theme_gtsum <- get_gtsummary_theme()
+theme_gtsum$`pkgwide-str:theme_name` = "Compact_custom"
+theme_gtsum$`tbl_summary-fn:addnl-fn-to-run` = bold_labels
+theme_gtsum$`tbl_svysummary-fn:addnl-fn-to-run` = bold_labels
+theme_gtsum$`tbl_regression-fn:addnl-fn-to-run` = bold_labels
+check_gtsummary_theme(theme_gtsum)
+set_gtsummary_theme(theme_gtsum)
+
+library(gt)
+
+# Mise en forme graphiques
+library(ggplot2)
+theme_get()
+theme_set(theme_minimal())
