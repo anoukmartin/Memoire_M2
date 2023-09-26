78bf18ce764ece9493dbdd5616abd609ff82100a
anoukmartin <anouk.mrtn@gmail.com>
2023-09-26 12:19:57 +0000

    Ajout d'un script d'execution des autres scripts

---
+++ Code/Execution.R
@@ -0,0 +1,13 @@
+
+# Chemin des scripts R 
+codePath <- "Code"
+
+# Table des scripts qu'on veut exécuter dans l'ordre : 
+listScript <- list.files(codePath) 
+listScript <- listScript[1:3]
+listScript
+
+for (script in listScript) {
+  print(paste0("Execution du script : ", script))
+  source(file.path(codePath, script))
+}
