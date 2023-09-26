6c8d92bf3a8ff50ec53fc218006829c644fee280
anoukmartin <anouk.mrtn@gmail.com>
2023-09-26 13:19:57 +0000

    Ajout d'un script renseignant les métadonnées de l'enquète BDF 2017

---
+++ Code/03_metadonnees.R
@@ -0,0 +1,6 @@
+
+infosBDF <- NULL
+infosBDF$des <- "Métadonnées de l'enquête budget de famille"
+infosBDF$nom <- "Budget de famille"
+infosBDF$champ <- "ménages ordinaires résidant en France"
+infosBDF$vague <- 2017
