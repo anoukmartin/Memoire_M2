6770327d4a35b05e65350d629b2b8036736cadca
anoukmartin <anouk.mrtn@gmail.com>
2023-10-24 06:29:21 +0000

    ajout fin du script

--- Code/03c_reperage_sous-pop_menages.R
+++ Code/03c_reperage_sous-pop_menages.R
@@ -7,14 +7,17 @@
 # situations des enfants on identifie quels sont les ménages avec une configuation 
 # familiale recomposée
 
+# On charge les données 
 enfantsTous <- readRDS("Data_output/enfantsTous.Rds") 
 menages <- readRDS("Data_output/menages.Rds")
 
+# On ne garde que les enfants vivant au domicile 
 enfantsD <- enfantsTous %>%
   filter(n_statutResid == "Enfant du ménage (au sens du TCM)") %>%
   mutate(n_IdentMenage = str_sub(n_IdentIndiv, 1, 5)) %>%
   mutate(n_NOI = str_sub(n_IdentIndiv, 6, 7)) 
 
+# tableau des configuration
 config <- enfantsD %>%
   pivot_wider(id_cols = n_IdentMenage, 
               names_from = n_NOI, 
@@ -22,9 +25,11 @@ config <- enfantsD %>%
               values_from = n_configFamEnfantsPSexe) %>%
   mutate(across(.cols = starts_with("EnfantNOI_"), 
                 .fns = function(x){
-                  x = labelled(x, label = "Configuration familial de l'enfant de NOI")
+                  x = labelled(x, label = "Configuration familiale de l'enfant de NOI")
                 })) 
 
+# On essaye de synthétiser par ménage (prendre en compte la situation de tous les 
+# enfants du ménages)
 configSynthese <- enfantsD %>%
   group_by(n_IdentMenage) %>%
   reframe(mods = unique(n_configFamEnfantsPSexe)) %>%
@@ -40,7 +45,11 @@ configSynthese <- configSynthese %>%
               names_from = mods, values_fill = F) %>% 
   left_join(compte)
 
+rm(compte)
+# On crée deux variable : 
+## - une de configuration familiale :
 configSynthese$n_configFam <- NA
+## - indicatrice d'enfants d'une nouvelle union :
 configSynthese$n_enfantNewUnion <- NA
 
 # Pour tous les ménages pour lesquels on a une unique configuration renseignée, 
@@ -51,63 +60,139 @@ configSynthese[
   configSynthese$n_config == 1, "names_config"
 ]
 
-# Pour ceux qui sont recomposés, on ajoute qu'il n'y a pas d'enfant de la nouvelle union 
+# Pour ceux qui sont recomposés, on ajoute qu'il n'y a pas d'enfant de la 
+# nouvelle union (sinon, on aurait eu également "configuration traditionnelle")
+# de renseignée 
 configSynthese[
-  configSynthese$`Configuration recomposée maternelle` | configSynthese$`Configuration recomposée paternelle` ,
+  configSynthese$n_config == 1 
+  & (configSynthese$`Configuration recomposée maternelle` | configSynthese$`Configuration recomposée paternelle`),
   "n_enfantNewUnion"
-] <- F
+] <- 0
 
+# Pour ceux qui sont tard, on ajoute qu'il y des enfants de la 
+# nouvelle union
+configSynthese[
+  configSynthese$n_config == 1 & configSynthese$`Configuration traditionelle`,
+  "n_enfantNewUnion"
+] <- 1
 
 # Pour les cas ou on a plusieurs configurations 
+## Cas de famille recomposées par les deux membres du couple
 configSynthese[
   configSynthese$n_config == 2 
   & configSynthese$`Configuration recomposée maternelle` 
   & configSynthese$`Configuration recomposée paternelle`, 
   c("n_configFam", "n_enfantNewUnion")
-] <- list("Configuration recomposée maternelle et paternelle", F)
+] <- list("Configuration recomposée maternelle et paternelle", 0)
 
+## Cas de famille recomposée par une femme avec enfant d'une nouvelle union 
 configSynthese[
   configSynthese$n_config == 2 
   & configSynthese$`Configuration recomposée maternelle` 
   & configSynthese$`Configuration traditionelle`,
   c("n_configFam", "n_enfantNewUnion")
-] <- list("Configuration recomposée maternelle", T)
+] <- list("Configuration recomposée maternelle", 1)
 
+## Cas de famille recomposée par un homme avec enfant d'une nouvelle union 
 configSynthese[
   configSynthese$n_config == 2 
   & configSynthese$`Configuration recomposée paternelle` 
   & configSynthese$`Configuration traditionelle`,
   c("n_configFam", "n_enfantNewUnion")
-] <- list("Configuration recomposée paternelle", T)
+] <- list("Configuration recomposée paternelle", 1)
 
+
+## Cas de famille recomposée par les deux conjoints avec enfant de la nouvelle
+# union
 configSynthese[
   configSynthese$n_config == 3 
   & configSynthese$`Configuration recomposée paternelle` 
   & configSynthese$`Configuration traditionelle`
   & configSynthese$`Configuration recomposée maternelle`,
   c("n_configFam", "n_enfantNewUnion")
-] <- list("Configuration recomposée maternelle et paternelle", T)
+] <- list("Configuration recomposée maternelle et paternelle", 1)
 
+# On a plusieurs ménages avec des configurations a priori incohérentes :
+# ex : monoparentale paternelle et recomposée maternelle 
 tab <- configSynthese[
   is.na(configSynthese$n_configFam), ]
+# Mais il peut s'agir de ménages ou il y a plusieurs couples et/ou des enfants 
+# de plusieurs générations. On va regarder :
+tab <- tab |>
+  left_join(menages |>
+              select("IDENT_MEN", "NCOUPLES", "NENFANTS", "TYPMEN5"), 
+            by = c("n_IdentMenage" = "IDENT_MEN")) |> 
+  rec_TYPMEN5()
+freq(tab$TYPMEN5)
+# Ce sont tous des ménages complexes, contrairement à la plus part des autres
+configSynthese |>
+  left_join(menages |>
+              select("IDENT_MEN", "NCOUPLES", "NENFANTS", "TYPMEN5"), 
+            by = c("n_IdentMenage" = "IDENT_MEN")) |> 
+  rec_TYPMEN5() |> 
+  select(n_configFam, n_config, TYPMEN5) |>
+  tbl_summary(by = "TYPMEN5")
 
+# Donc on va recoder en considérant que la structure de ces familles ne s'assimile
+# pas aux catégories proposées 
+configSynthese[
+  is.na(configSynthese$n_configFam), "n_configFam" ] <- "Configuration complexe"
 
+rm(tab) #cleanning 
 
+# on a également un cas avec monoparentale ou recomposée non identifiée parce que 
+# le statut conjugale de la mère était manquant;
+tab <- configSynthese[
+  configSynthese$n_configFam == "Configuration monoparentale ou recomposée maternelle", ]
+# on regarde les infos qu'on a sur ce ménage
+tab <- tab |>
+  left_join(menages |>
+              select("IDENT_MEN", "NCOUPLES", "NENFANTS", "TYPMEN5"), 
+            by = c("n_IdentMenage" = "IDENT_MEN")) |> 
+  rec_TYPMEN5()
+# Il s'agit d'un ménage complexe avec aucun couple et un enfant, on peut supposer 
+# qu'il s'assimile d'avantage une famille monoparentale
+# On recode en config mono maternelle
+configSynthese[
+  configSynthese$n_configFam == "Configuration monoparentale ou recomposée maternelle", "n_configFam" ] <- "Configuration monoparentale maternelle"
+rm(tab)
 
-configMatrice <- config
-configMatrice <- as.matrix(configMatrice)
+# On va ajouter un recodage qui distingue configuration et genre de la configuration 
+freq(configSynthese$n_configFam)
+configSynthese <- configSynthese %>%
+  mutate(
+    n_genreFam = case_when(
+      n_configFam %in% c("Configuration complexe") ~ NA, 
+      n_configFam %in% c("Configuration traditionelle") ~ "Les deux",
+      n_configFam %in% c("Configuration monoparentale maternelle", "Configuration recomposée maternelle") ~ "Femme", 
+      n_configFam %in% c("Configuration monoparentale paternelle", "Configuration recomposée paternelle") ~ "Homme", 
+      n_configFam == "Configuration recomposée maternelle et paternelle" ~ "Les deux"),
+    n_config = case_when(
+      n_configFam == "Configuration complexe" ~ "Complexe",
+      n_configFam == "Configuration traditionelle" ~ "Traditionelle",
+      n_configFam %in% c("Configuration monoparentale maternelle", "Configuration monoparentale paternelle") ~ "Monoparentale", 
+      n_configFam %in% c("Configuration recomposée maternelle", "Configuration recomposée paternelle", "Configuration recomposée maternelle et paternelle") ~ "Recomposée"))
 
-  filter(is.na(EnfantNOI_03))
-  filter(EnfantNOI_03 != EnfantNOI_04
-         |EnfantNOI_03 != EnfantNOI_04
-           )
+# on supprime cette variable peu utile
+configSynthese$names_config <- NULL
+# On labelise les variables synthétiques
+configSynthese$n_config <- labelled(configSynthese$n_config, 
+                                    label = "Configuration familiale")
+configSynthese$n_genreFam <- labelled(configSynthese$n_genreFam, 
+                                    label = "Sexe des parents")
+configSynthese$n_enfantNewUnion <- labelled(configSynthese$n_enfantNewUnion, 
+                                    label = "Existance d'enfants issus de l'union actuelle")
 
+configSynthese %>%
+  select(n_config, n_genreFam, n_enfantNewUnion) %>%
+  tbl_summary(by = "n_config")
 
 familles <- menages %>%
-  right_join(config, by = c("IDENT_MEN" = "n_IdentMenage"))
+  right_join(config, by = c("IDENT_MEN" = "n_IdentMenage")) %>%
+  left_join(configSynthese,  by = c("IDENT_MEN" = "n_IdentMenage"))
 
+saveData(familles, label = "familles")
 
-
-
+rm(config, configSynthese, dictionnaire, menages, enfantsD, enfantsTous, familles)
 
 
