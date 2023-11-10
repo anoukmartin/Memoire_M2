b831cbbd9f799ba1582b42c4007f4910a0fd235e
anoukmartin <anouk.mrtn@gmail.com>
2023-11-10 20:29:03 +0000

    source

--- Code/Execution.R
+++ Code/Execution.R
@@ -18,8 +18,10 @@ execute <- function(part, path = "Code"){
 
 
 # Au démarrage #################################################################
-# On exécute les scripts commençant par 01
-execute(part = "01")
+# On exécute les scripts commençant par 00 et 01
+source(file = "Code/00_requirements.R")
+source(file = "Code/01a_packages.R")
+source(file = "Code/01b_fonctions.R")
 
 # Réimporter les données brutes ################################################
 # On exécute les scripts commençant par 02
