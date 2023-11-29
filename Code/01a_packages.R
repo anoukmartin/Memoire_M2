202f16910754c28d14bb9cbb93c5c487c0a3c16f
anoukmartin <anouk.mrtn@gmail.com>
2023-11-29 14:01:41 +0000

    commentaires du code

--- Code/01a_packages.R
+++ Code/01a_packages.R
@@ -1,15 +1,30 @@
+
+# Git et gestion du coffre aws
+library(git2r)
+library(aws.s3)
+
+# Packages Rmd 
+library(rmarkdown)
+library(knitr)
+# On met cette fonction en français
+combine_words <- function(words){ 
+  str <- combine_words(words, and = " et ", oxford_comma = FALSE)
+  return(str)
+}
+library(pagedown)
+
+# from sas to R, vecteurs labelisés
 library(haven)
 library(labelled)
 
-library(git2r)
-library(stringr)
-library(aws.s3)
-
+# manipulation de données
+library(tidyverse)
 library(dplyr)
 library(forcats)
 library(tidyr)
+library(stringr)
 
-library(questionr)
+# Données pondérées et plans de sondage complexes
 library(survey)
 library(srvyr)
 
@@ -23,11 +38,22 @@ theme_gtsum$`pkgwide-str:theme_name` = "Compact_custom"
 theme_gtsum$`tbl_summary-fn:addnl-fn-to-run` = bold_labels
 theme_gtsum$`tbl_svysummary-fn:addnl-fn-to-run` = bold_labels
 theme_gtsum$`tbl_regression-fn:addnl-fn-to-run` = bold_labels
-theme_gtsum$`tbl_summary-str:continuous_stat` = c("{mean}", "{p25} - {p75}", "{min} - {max}")
+theme_gtsum$`tbl_summary-str:continuous_stat` = c("{mean}", 
+                                                  "{p25} - {p75}", 
+                                                  "{min} - {max}")
+theme_gtsum$`tbl_summary-str:categorical_stat` = "{p} ({n})"
+
+# theme_gtsum$`tbl_svysummary-str:continuous_stat` = c("{mean}", 
+#                                                     "{p25} - {p75}", 
+#                                                     "{min} - {max}")
+# theme_gtsum$`tbl_svysummary-arg:statistics` = "{p} ({n_unweighted})"
+
 check_gtsummary_theme(theme_gtsum)
 set_gtsummary_theme(theme_gtsum)
 rm(theme_gtsum)
 
+
+library(questionr)
 library(gt)
 library(flextable)
 
@@ -36,12 +62,8 @@ library(ggplot2)
 theme_get()
 theme_set(theme_minimal())
 
-# Packages Rmd 
-library(rmarkdown)
-library(knitr)
-# On met cette fonction en français
-combine_words <- combine_words(words, and = " et ", oxford_comma = FALSE)
-library(pagedown)
+
+
 
 
 
