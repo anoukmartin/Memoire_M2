ac8f1311fbe0ac99e20e39e3f2cc46bde81e6573
anoukmartin <anouk.mrtn@gmail.com>
2023-11-30 12:57:36 +0000

    deplacement du fichier à la racine du projet

---
+++ requirements.R
@@ -0,0 +1,14 @@
+packagesList <- c("haven",
+                 "labelled", 
+                 "git2r", 
+                 "tidyverse", 
+                 "questionr",
+                 "gtsummary",
+                 "gt", 
+                 "rmarkdown", 
+                 "knitr", 
+                  "survey", 
+                  "srvyr", 
+                  "flextable", 
+                  "pagedown")
+install.packages(packagesList)
